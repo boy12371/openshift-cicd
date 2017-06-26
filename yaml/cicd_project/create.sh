003cd6dd1e8e7ad89a47908e93b50f05b9ab33e4
boy12371 <boy12371@hotmail.com>
2017-06-27 02:47:51 +0800

    20170627024751

--- yaml/cicd_project/create.sh
+++ yaml/cicd_project/create.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #
-if $(oc whoami) -ne system:admin; then
-  echo "Create these apps by user system:admin."
+projectName=cicd
+if [ "$(oc whoami)" != "system:admin" ]; then
+  echo "Please create these apps by user system:admin."
   exit 0
-else if $(oc project -q) -ne cicd; then
-  echo "Using project cicd create these apps."
+elif [ "$(oc project -q)" != "cicd" ]; then
+  echo "Please using project cicd create these apps."
   exit 0
 else
-  oc process -f cicd-global-template.yaml |oc create -f -
-  oc process -f cicd-gogs-persistent-template.yaml |oc create -f -
-# oc process -f cicd-nexus-persistent-template.yaml |oc create -f -
-# oc process -f cicd-subversion-persistent-template.yaml |oc create -f -
-# oc process -f cicd-sonarqube-persistent-template.yaml |oc create -f -
+  oc process -f cicd-global-template.yaml |oc create -f - -n $projectName
+  oc process -f cicd-gogs-persistent-template.yaml |oc create -f - -n $projectName
+# oc process -f cicd-nexus-persistent-template.yaml |oc create -f - -n $projectName
+# oc process -f cicd-subversion-persistent-template.yaml |oc create -f - -n $projectName
+# oc process -f cicd-sonarqube-persistent-template.yaml |oc create -f - -n $projectName
 fi
--- yaml/cicd_project/remove_bash/remove_gogs.sh
+++ yaml/cicd_project/remove_bash/remove_gogs.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 #
-oc delete all -l app=gogs -n cicd
+projectName=cicd
+if [ "$(oc whoami)" != "system:admin" ]; then
+  echo "Please create these apps by user system:admin."
+  exit 0
+elif [ "$(oc project -q)" != "cicd" ]; then
+  echo "Please using project cicd create these apps."
+  exit 0
+else
+oc delete all -l app=gogs -n $projectName
 # oc delete rolebinding/default_edit -n cicd
-# oc delete pv/cicd-gogs-postgresql-pv
-# oc delete pvc/gogs-postgresql-data -n cicd
-# oc delete pv/cicd-gogs-pv
-# oc delete pvc/gogs-data -n cicd
-oc delete events --all -n cicd
+oc delete pv/cicd-gogs-postgresql-pv
+oc delete pvc/gogs-postgresql-data -n $projectName
+oc delete pv/cicd-gogs-pv
+oc delete pvc/gogs-data -n $projectName
+oc delete configmap/gogs-config -n $projectName
+oc delete events --all -n $projectName
