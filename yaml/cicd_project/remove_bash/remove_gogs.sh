8f87be2d7022de68258976591b1cf56621225164
boy12371 <boy12371@hotmail.com>
2017-06-27 01:17:41 +0800

    20170627011741

--- yaml/cicd_project/remove_bash/remove_gogs.sh
+++ yaml/cicd_project/remove_bash/remove_gogs.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #
 oc delete all -l app=gogs -n cicd
-#oc delete rolebinding/default_edit -n cicd
-oc delete pv/cicd-gogs-postgresql-pv
-oc delete pvc/zentao-mysql-data -n product
-oc delete pv/product-zentao-pv
-oc delete pvc/zentao-data -n product
-oc delete secrets/zentao -n product
-oc delete events --all -n product
+# oc delete rolebinding/default_edit -n cicd
+# oc delete pv/cicd-gogs-postgresql-pv
+# oc delete pvc/gogs-postgresql-data -n cicd
+# oc delete pv/cicd-gogs-pv
+# oc delete pvc/gogs-data -n cicd
+oc delete events --all -n cicd
