5ed2ba09e4378491d0e9710b7831a325af5333f1
boy12371 <boy12371@hotmail.com>
2017-06-27 02:01:05 +0800

    20170627020105

--- yaml/cicd_project/create.sh
+++ yaml/cicd_project/create.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 #
-oc process -f cicd-global-template.yaml |oc create -f -
-oc process -f cicd-gogs-persistent-template.yaml |oc create -f -
+if $(oc whoami) -ne system:admin; then
+  echo "Create these apps by user system:admin."
+  exit 0
+else if $(oc project -q) -ne cicd; then
+  echo "Using project cicd create these apps."
+  exit 0
+else
+  oc process -f cicd-global-template.yaml |oc create -f -
+  oc process -f cicd-gogs-persistent-template.yaml |oc create -f -
 # oc process -f cicd-nexus-persistent-template.yaml |oc create -f -
 # oc process -f cicd-subversion-persistent-template.yaml |oc create -f -
 # oc process -f cicd-sonarqube-persistent-template.yaml |oc create -f -
+fi
