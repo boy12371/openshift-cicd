0d9537a1db012cc09557132946aa4ee2609d8307
boy12371 <boy12371@hotmail.com>
2017-06-27 01:17:57 +0800

    20170627011757

--- yaml/cicd_project/create.sh
+++ yaml/cicd_project/create.sh
@@ -2,6 +2,6 @@
 #
 oc process -f cicd-global-template.yaml |oc create -f -
 oc process -f cicd-gogs-persistent-template.yaml |oc create -f -
-oc process -f cicd-nexus-persistent-template.yaml |oc create -f -
-oc process -f cicd-subversion-persistent-template.yaml |oc create -f -
-oc process -f cicd-sonarqube-persistent-template.yaml |oc create -f -
+# oc process -f cicd-nexus-persistent-template.yaml |oc create -f -
+# oc process -f cicd-subversion-persistent-template.yaml |oc create -f -
+# oc process -f cicd-sonarqube-persistent-template.yaml |oc create -f -
