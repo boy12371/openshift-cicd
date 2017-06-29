9151627a5ab534f30c680ef7b47799ba9e0ec4e5
boy12371 <boy12371@hotmail.com>
2017-06-30 06:53:01 +0800

    20170630065301

--- yaml/product_project/remove_bash/remove_nginx.sh
+++ yaml/product_project/remove_bash/remove_nginx.sh
@@ -2,6 +2,8 @@
 #
 oc delete all -l app=nginx -n product
 #oc delete rolebinding/default_edit -n product
-oc delete pv/product-nginx-pv
-oc delete pvc/nginx-data -n product
+oc delete pv/product-nginx-conf-pv
+oc delete pvc/nginx-conf -n product
+oc delete pv/product-nginx-html-pv
+oc delete pvc/nginx-html -n product
 oc delete events --all -n product
