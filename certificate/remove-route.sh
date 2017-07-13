dca088cd969a199b95e80cd6b5fb8aefffeac64a
boy12371 <boy12371@hotmail.com>
2017-07-14 07:22:06 +0800

    20170714072206

--- certificate/remove-route.sh
+++ certificate/remove-route.sh
@@ -4,5 +4,5 @@ if [ "$(oc whoami)" != "system:admin" ]; then
   oc login -u system:admin -n $PROJECT3
 fi
 for ((c=1;c<101;c++)); do
-  oc delete route/`eval echo '$DNNS'$c` -n `eval echo '$PROJ'$c`
+  oc delete route/`eval echo '$DNNSNAME'$c` -n `eval echo '$PROJ'$c`
 done
