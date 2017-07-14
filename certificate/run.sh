da5fadfd62c12ac79d1b28cb95f949ce4cc1fd21
boy12371 <boy12371@hotmail.com>
2017-07-14 13:21:04 +0800

    20170714132104

--- certificate/run.sh
+++ certificate/run.sh
@@ -9,15 +9,19 @@ for ((i=1;i<5;i++)); do
   DOMAIN=`eval echo '$DOMAIN'$i`
   . "./dns-variable.sh" $DOMAIN
   . "./create-cnf.sh" $DOMAIN
-  KEY=$DOMAIN'.key'
+  ACCOUNTKEY='account.key'
+  DOMAINKEY=$DOMAIN'.key'
   CSR=$DOMAIN'.csr'
   UNSIGN_CRT=$DOMAIN'.unsigned.crt'
   SIGN_CRT=$DOMAIN'.signed.crt'
+  if [ ! -f "$ACCOUNTKEY"]; then
+    openssl genrsa 4096 > $ACCOUNTKEY
+  fi
   if [ ! -f "$UNSIGN_CRT" ]; then
-    openssl req -out $CSR -newkey rsa:2048 -nodes -keyout $KEY -config $CNF
-    openssl x509 -req -days 366 -in $CSR -signkey $KEY -out $UNSIGN_CRT -extensions req_ext -extfile $CNF
+    openssl req -out $CSR -newkey rsa:4096 -nodes -keyout $DOMAINKEY -config $CNF
+    openssl x509 -req -days 366 -in $CSR -signkey $DOMAINKEY -out $UNSIGN_CRT -extensions req_ext -extfile $CNF
   # cp ~/certificate/zhonglele.com.crt /etc/origin/master/
-  # cp ~/certificate/$KEY /etc/origin/master/
+  # cp ~/certificate/$DOMAINKEY /etc/origin/master/
   # wget https://raw.githubusercontent.com/boy12371/acme-tiny/master/acme_tiny.py
   fi
 
@@ -25,7 +29,7 @@ for ((i=1;i<5;i++)); do
   . "./install-nginx-route.sh"
 
   if [ ! -f "$SIGN_CRT" ]; then
-    python acme_tiny.py --account-key ./$KEY --csr ./$CSR --acme-dir $ACME_DIR > ./$SIGN_CRT || exit
+    python acme_tiny.py --account-key ./$ACCOUNTKEY --csr ./$CSR --acme-dir $ACME_DIR > ./$SIGN_CRT || exit
   fi
 
   . "./remove-nginx-route.sh"
